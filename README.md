6fcff4c51c94ae3f96ce313ae10a325a1024c8a4
Margo Seltzer <margoseltzer@gmail.com>
2017-11-03 11:15:17 -0400

    First attempt at homebrew packaging.

---
+++ README.md
@@ -0,0 +1,8 @@
+# corels-brew
+Brew files for installation of corels (certifiably optimal rule lists).
+See: 
+
+Brief video overview: https://www.youtube.com/watch?v=ebJHnDLLTKA
+Github repo: https://github.com/nlarusstone/corels
+KDD Paper: https://dl.acm.org/citation.cfm?id=3098047
+arXiv Paper: https://arxiv.org/abs/1704.01701
---
+++ corels.rb
@@ -0,0 +1,26 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Sbrlmod < Formula
+  desc "Scalable Bayesian Rule Lists. C-library to build a scalable rule list from input data and labels."
+  homepage "https://github.com/margoseltzer/corels-homebrew/wiki"
+  url "https://github.com/margoseltzer/corels-homebrew/files/1441466/corels-1.0.tar.gz"
+  version "1.0"
+  sha256 "013fd81472759a4e8f7ae8dec9ce4a9769286d41cd3ff33c5fc9bfb668a70b7d"
+  # depends_on "cmake" => :build
+  depends_on "gsl"
+  depends_on "gmp"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/corels", "-b", "#{share}/corels/compas_test.out", "#{share}/corels/compas_test.label"
+  end
+end
